3676875810261bd5eb7c4c3c76dc634edb6fd7b5
Mark Miyashita <negativetwelve@gmail.com>
2013-03-29 13:22:30 -0700

    reverting fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    session[:user_id] = nil
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     #@current_user = User.first
   end
